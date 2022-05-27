ec93afb46a5cf5fdb3902150b8b238f68bba820d
Kretchen001 <tim-kretzschmar@t-online.de>
2022-05-27 18:26:02 +0200

    Project erstellt

---
+++ Anicluster/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+namespace Anicluster {
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+
+        public List<AnimeRow> listOfAnimeRows = new List<AnimeRow>();
+
+        public MainWindow() {
+            InitializeComponent();
+
+        }
+}
