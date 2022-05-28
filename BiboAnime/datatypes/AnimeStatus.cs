4dfade406d4cafe044b709abe73f58ae59111979
Kretchen001 <tim-kretzschmar@t-online.de>
2022-05-28 12:21:11 +0200

    Enum mit Staten, die ein Anime haben können soll

---
+++ BiboAnime/datatypes/AnimeStatus.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.Serialization;
+
+namespace BiboAnime.datatypes {
+
+    public enum AnimeStatus {
+
+        [EnumMember(Value = "want")]
+        Wunschliste,
+        [EnumMember(Value = "started")]
+        Angefangen,
+        [EnumMember(Value = "finish")]
+        Fertig,
+        [EnumMember(Value = "stalled")]
+        Unterbrochen,
+        [EnumMember(Value = "dropped")]
+        Abgebrochen
+    }
+}
