962851ac427de71f4ade5f21f48ce79d05b33b46
Kretchen001 <tim-kretzschmar@t-online.de>
2022-05-28 12:20:36 +0200

    Fenster erstellt
    - Fenster für die Details eines Animes erstellt
    - Bisher nur Dummy-Fenster

---
+++ Anicluster/windows/ShowDetails.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace Anicluster.windows {
+    /// <summary>
+    /// Interaktionslogik für ShowDetails.xaml
+    /// </summary>
+    public partial class ShowDetails : Window {
+
+        public ShowDetails() {
+            InitializeComponent();
+        }
+
+        private void click_EditEnitity(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void click_RemoveEntity(object sender, RoutedEventArgs e) {
+
+        }
+    }
+}
