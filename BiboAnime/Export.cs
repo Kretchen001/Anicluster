672c6c899bbe279fbb259eb953c48ce95498dc56
Kretchen001 <tim-kretzschmar@t-online.de>
2022-05-28 12:17:25 +0200

    Exp und Imp vorbereiter
    Noch reine Dummy-Funktionen

---
+++ BiboAnime/Export.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiboAnime {
+
+    public static class Export {
+
+        public static bool exportAnimeList() {
+
+            return false;
+        }
+
+        public static bool exportAnimeTags() {
+
+            return false;
+        }
+    }
+}
---
+++ BiboAnime/Import.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiboAnime {
+
+    public static class Import {
+
+        public static bool importAnimeList() {
+
+            return false;
+        }
+
+        public static bool importAnimeTags() {
+
+            return false;
+        }
+    }
+}
