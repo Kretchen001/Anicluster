938d9d72e2b65e9db8cb8696a1f514fbe4a9e422
Kretchen001 <tim-kretzschmar@t-online.de>
2022-05-28 12:17:49 +0200

    Create data.cs
    Auslagerung der Daten, auf die von mehreren Stellen zugegriffen werden muss

---
+++ Anicluster/data.cs
@@ -0,0 +1,13 @@
+﻿using BiboAnime.datatypes;
+using System.Collections.Generic;
+
+namespace Anicluster {
+
+    public struct data {
+
+        public static List<AnimeRow> listOfAnimeRows = new List<AnimeRow>();
+
+        public static List<AnimeData> listOfAnimes = new List<AnimeData>();
+
+    }
+}
