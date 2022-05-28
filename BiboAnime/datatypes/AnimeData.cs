28cd8f8720117a8efec1848c2736b03bf12929b8
Kretchen001 <tim-kretzschmar@t-online.de>
2022-05-28 12:22:19 +0200

    Die wirklich harten Daten eines Animes
    1. Version, damit schonmal testbar
    wird sicher noch mehr

---
+++ BiboAnime/datatypes/AnimeData.cs
@@ -0,0 +1,16 @@
+﻿namespace BiboAnime.datatypes {
+
+    public class AnimeData {
+        
+        public int id { get; set; }
+        public bool favorite { get; set; }
+        public string name { get; set; }
+        public string originalName { get; set; }
+        public int rating { get; set; }
+        public List<string> tags { get; set; }
+        public string urlAnimePlanet { get; set; }
+        public AnimeStatus status { get; set; }
+        public int staffeln { get; set; }
+        public int episodes { get; set; }
+    }
+}
