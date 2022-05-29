28bd9eeb4ce7a7a52951b44ba9e1954bf5538c56
Kretchen001 <tim-kretzschmar@t-online.de>
2022-05-29 21:56:12 +0200

    Details-Window erweitert
    - label-Namen damit Inhalt angezeigt wird
    - Favorit Zwangslösung geschrieben

--- Anicluster/windows/ShowDetails.xaml.cs
+++ Anicluster/windows/ShowDetails.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using BiboAnime.datatypes;
 using System.Windows;
+using System.Windows.Media;
 
 namespace Anicluster.windows {
     /// <summary>
@@ -8,10 +9,26 @@ namespace Anicluster.windows {
     public partial class ShowDetails : Window {
 
         public AnimeData oneAnime;
+        private bool isFavorite;
 
         public ShowDetails(AnimeData oneAnime) {
             InitializeComponent();
             this.oneAnime = oneAnime;
+
+            labelName.Content = oneAnime.name;
+            labelOriginalName.Content = oneAnime.originalName;
+            labelRatingGeneral.Content = oneAnime.rating.general;
+            labelFavorite.Content = oneAnime.favorite.ToString();
+
+            isFavorite = oneAnime.favorite;
+            if (isFavorite) {
+                labelFavorite.Foreground = new SolidColorBrush(Colors.DarkGreen);
+                labelFavorite.Content = "Favorisiert";
+            }
+            else {
+                labelFavorite.Foreground = new SolidColorBrush(Colors.DarkRed);
+                labelFavorite.Content = "Kein Favorit";
+            }
         }
 
         private void click_EditEnitity(object sender, RoutedEventArgs e) {
@@ -20,6 +37,23 @@ namespace Anicluster.windows {
 
         private void click_RemoveEntity(object sender, RoutedEventArgs e) {
 
+        }
+
+        private void click_ShowRatingDetails(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void click_DoubleClickChangeFavorite(object sender, System.Windows.Input.MouseButtonEventArgs e) {
+            if (isFavorite) {
+                isFavorite = false;
+                labelFavorite.Foreground = new SolidColorBrush(Colors.DarkRed);
+                labelFavorite.Content = "Kein Favorit";
+            }
+            else {
+                isFavorite = true;
+                labelFavorite.Foreground = new SolidColorBrush(Colors.DarkGreen);
+                labelFavorite.Content = "Favorisiert";
+            }
         }
     }
 }
