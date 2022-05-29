91a9893ffeab190671dddc9bcd98a587277477c0
Kretchen001 <tim-kretzschmar@t-online.de>
2022-05-29 21:50:51 +0200

    "Setter" vom Rating funktionsfähig umgeschrieben

--- BiboAnime/datatypes/AnimeData.cs
+++ BiboAnime/datatypes/AnimeData.cs
@@ -6,11 +6,37 @@
         public bool favorite { get; set; }
         public string name { get; set; }
         public string originalName { get; set; }
-        public int rating { get; set; }
+        public Rating rating { get; set; }
         public List<string> tags { get; set; }
         public string urlAnimePlanet { get; set; }
         public AnimeStatus status { get; set; }
         public int staffeln { get; set; }
         public int episodesTotal { get; set; }
+    }
+
+    public class Rating {
+
+        public int story { get; set; }
+        public int sound { get; set; }
+        public int animation { get; set; }
+        public int specialEffect { get; set; }
+        public int germanDub { get; set; }
+        public int general { get; set; }
+
+        public Rating(int story, int sound, int animation, int specialEffect, int germanDub) {
+            this.story = story;
+            this.sound = sound;
+            this.animation = animation;
+            this.specialEffect = specialEffect;
+            this.germanDub = germanDub;
+
+            double x = (story * 0.4) + (sound * 0.2) + (animation * 0.3) + (specialEffect * 0.1);
+            if (germanDub == 0) {
+                this.general = (int) ((story * 0.4) + (sound * 0.2) + (animation * 0.3) + (specialEffect * 0.1));
+            }
+            else {
+                this.general = (int) ((story * 0.4) + (sound * 0.1) + (animation * 0.3) + (specialEffect * 0.1) + (germanDub * 0.1));
+            }
+        }
     }
 }
