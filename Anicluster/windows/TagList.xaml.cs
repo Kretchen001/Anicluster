97d85cd3f8c99e2602d1f87a09c129c60e76caef
Kretchen001 <tim-kretzschmar@t-online.de>
2022-06-12 20:29:52 +0200

    Tag-Verwaltung angefangen

---
+++ Anicluster/windows/TagList.xaml.cs
@@ -0,0 +1,21 @@
+﻿using BiboAnime.datatypes;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace Anicluster.windows {
+    /// <summary>
+    /// Interaktionslogik für TagList.xaml
+    /// </summary>
+    public partial class TagList : Window {
+
+        public TagList(List<AnimeTag> tagList) {
+            InitializeComponent();
+
+            dataGridTagList.ItemsSource = tagList;
+        }
+
+        private void clickConfirmTagChoice(object sender, RoutedEventArgs e) {
+
+        }
+    }
+}
