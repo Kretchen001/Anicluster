4f8c4adec710b2d56878c58226294f9cc0806e20
Kretchen001 <tim-kretzschmar@t-online.de>
2022-06-15 22:18:23 +0200

    Update databaseFilterController.cs

--- BiboAnime/databaseLiteDB/databaseFilterController.cs
+++ BiboAnime/databaseLiteDB/databaseFilterController.cs
@@ -5,32 +5,31 @@ namespace BiboAnime.databaseLiteDB {
 
     public class databaseFilterController {
 
-        public List<AnimeFilterTier> dbFilterTier(AnimeTier tierToFilter) {
+        public List<AnimeData> dbFilterTier(AnimeTier tierToFilter) {
             using (var db = new LiteDatabase(databaseConfigs.locationOfDataBase)) {
                 var col = db.GetCollection<AnimeData>("Animes");
                 var result = col.Query()
                     .Where(x => x.tier == tierToFilter)
                     .ToList();
-                List<AnimeFilterTier> filters = new List<AnimeFilterTier>();
+                return result;
+            }
+        }
+
+        public List<AnimeData> dbFilterTagAndTier(AnimeTier filterTier, List<AnimeTag> filterTags) {
+            using (var db = new LiteDatabase(databaseConfigs.locationOfDataBase)) {
+                var col = db.GetCollection<AnimeData>("Animes");
+                List<AnimeData> result = col.Query()
+                    .Where(x => x.tier == filterTier)   // filter tier | tags later
+                    .ToList();
+                List<AnimeData> filters = new List<AnimeData>();
                 for (int i = 0; i < result.Count; i += 1) {
-                    List<AnimeTag> tags = new List<AnimeTag>();
                     for (int j = 0; j < result[i].tags.Count; j += 1) {
-
+                        for (int k = 0; k < filterTags.Count; k += 1) {
+                            if (result[i].tags[j].tagDesignator == filterTags[k].tagDesignator) {
+                                filters.Add(result[i]);
+                            }
+                        }
                     }
-                    Rating rating = new Rating(
-                        result[i].rating.story,
-                        result[i].rating.sound,
-                        result[i].rating.animation,
-                        result[i].rating.specialEffect,
-                        result[i].rating.germanDub);
-
-                    filters.Add(new AnimeFilterTier() {
-                        id = i,
-                        tier = result[i].tier,
-                        name = result[i].name,
-                        rating = rating,
-                        animeTag = tags
-                    });
                 }
                 return filters;
             }
