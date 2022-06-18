ecbf29e7e1708f27bbc8938757e32faaa92dfdbc
Kretchen001 <tim-kretzschmar@t-online.de>
2022-06-18 17:35:45 +0200

    Show Details angefangen zu finalisieren für V1

--- Anicluster/windows/ShowDetails.xaml.cs
+++ Anicluster/windows/ShowDetails.xaml.cs
@@ -9,51 +9,33 @@ namespace Anicluster.windows {
     public partial class ShowDetails : Window {
 
         public AnimeData oneAnime;
-        private bool isFavorite;
+
+        private string a { get; set; } = "ahfsd";
 
         public ShowDetails(AnimeData oneAnime) {
             InitializeComponent();
             this.oneAnime = oneAnime;
-
-            labelName.Content = oneAnime.name;
-            labelOriginalName.Content = oneAnime.originalName;
-            labelRatingGeneral.Content = oneAnime.rating.general;
-            labelFavorite.Content = oneAnime.favorite.ToString();
-
-            isFavorite = oneAnime.favorite;
-            if (isFavorite) {
-                labelFavorite.Foreground = new SolidColorBrush(Colors.DarkGreen);
-                labelFavorite.Content = "Favorisiert";
-            }
-            else {
-                labelFavorite.Foreground = new SolidColorBrush(Colors.DarkRed);
-                labelFavorite.Content = "Kein Favorit";
-            }
+            labelNameHeadLine.Content = oneAnime.name;
         }
 
-        private void click_EditEnitity(object sender, RoutedEventArgs e) {
+        private void clickCallLink(object sender, RoutedEventArgs e) {
 
         }
 
-        private void click_RemoveEntity(object sender, RoutedEventArgs e) {
+        private void clickCloseButton(object sender, RoutedEventArgs e) {
 
         }
 
-        private void click_ShowRatingDetails(object sender, RoutedEventArgs e) {
+        private void clickEditEnitity(object sender, RoutedEventArgs e) {
 
         }
 
-        private void click_DoubleClickChangeFavorite(object sender, System.Windows.Input.MouseButtonEventArgs e) {
-            if (isFavorite) {
-                isFavorite = false;
-                labelFavorite.Foreground = new SolidColorBrush(Colors.DarkRed);
-                labelFavorite.Content = "Kein Favorit";
-            }
-            else {
-                isFavorite = true;
-                labelFavorite.Foreground = new SolidColorBrush(Colors.DarkGreen);
-                labelFavorite.Content = "Favorisiert";
-            }
+        private void clickRemoveEntity(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void clickChoosenTags(object sender, RoutedEventArgs e) {
+
         }
     }
 }
