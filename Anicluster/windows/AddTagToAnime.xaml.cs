927dfb8246653da5ee07a2782bc3546b2b9a80d8
Kretchen001 <83697846+Kretchen001@users.noreply.github.com>
2024-10-21 18:03:42 +0200

    Filter of Tags is now functional correct

--- Anicluster/windows/AddTagToAnime.xaml.cs
+++ Anicluster/windows/AddTagToAnime.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using Modells.Anime;
+using Modells.ViewModel;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace Anicluster.windows {
     /// <summary>
@@ -9,8 +12,9 @@ namespace Anicluster.windows {
     /// </summary>
     public partial class AddTagToAnime : Window {
 
-        public ObservableCollection<Tag> AllTags { get; set; } = new ObservableCollection<Tag>();
+        public ObservableCollection<TagViewModel> AllTags { get; set; } = new ObservableCollection<TagViewModel>();
         public ObservableCollection<Tag> SelectedTags { get; set; } = [];
+        public ICollectionView FilteredTags { get; set; }
 
         public AddTagToAnime(List<Tag>? tags = null) {
             if (tags is not null) {
@@ -20,27 +24,50 @@ namespace Anicluster.windows {
             }
             InitializeComponent();
             DataContext = this;
+            FilteredTags = CollectionViewSource.GetDefaultView(AllTags);
         }
 
         public bool IsTagSelected(Tag tag) {
             return SelectedTags.Contains(tag);
         }
 
-        public void ToggleTagSelection(Tag tag) {
-            if (SelectedTags.Contains(tag)) {
-                SelectedTags.Remove(tag);
+        public void ToggleTagSelection(TagViewModel tagViewModel) {
+            if (tagViewModel.IsSelected) {
+                tagViewModel.IsSelected = false;
+                SelectedTags.Remove(tagViewModel.Tag);
             }
             else {
-                SelectedTags.Add(tag);
+                tagViewModel.IsSelected = true;
+                SelectedTags.Add(tagViewModel.Tag);
             }
         }
 
         private void CheckBoxSelection_Click(object sender, RoutedEventArgs e) {
             CheckBox? checkBox = sender as CheckBox;
-            Tag? tag = checkBox?.DataContext as Tag;
-            if (tag is not null) {
-                ToggleTagSelection(tag);
+            TagViewModel? tagViewModel = checkBox?.DataContext as TagViewModel;
+            if (tagViewModel is not null) {
+                ToggleTagSelection(tagViewModel);
             }
+        }
+
+        public void FilterTags(string filter) {
+            FilteredTags.Filter = obj => {
+                if (obj is TagViewModel tagViewModel) {
+                    return string.IsNullOrEmpty(filter) || tagViewModel.Tag.Designation.Contains(filter, StringComparison.OrdinalIgnoreCase);
+                }
+                return false;
+            };
+            FilteredTags.Refresh();
+        }
+
+        private void TBFilter_TextChanged(object sender, TextChangedEventArgs e) {
+            TextBox? textBox = sender as TextBox;
+            string filter = textBox!.Text;
+            FilterTags(filter);
+        }
+
+        private void BtnFilterLeeren_Click(object sender, RoutedEventArgs e) {
+            TBFilter.Text = "";
         }
     }
 }
---
+++ Modells/ViewModel/TagViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Modells.Anime;
+
+namespace Modells.ViewModel {
+
+    public class TagViewModel {
+
+        public Tag Tag { get; set; }
+        public bool IsSelected { get; set; } = false;
+
+        public TagViewModel(Tag tag) {
+            Tag = tag;
+            IsSelected = false;
+        }
+    }
+}
