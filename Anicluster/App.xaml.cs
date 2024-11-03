9357e0127d6f0077d8e4a032063eddf887c75ed8
Kretchen001 <83697846+Kretchen001@users.noreply.github.com>
2024-11-03 15:59:14 +0100

    Use Serilog as logger

--- Anicluster/App.xaml.cs
+++ Anicluster/App.xaml.cs
@@ -1,12 +1,36 @@
-﻿using System.Configuration;
-using System.Data;
+using Serilog;
+using System.IO;
 using System.Windows;
 
 namespace Anicluster {
+
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application {
-    }
 
+        protected override void OnStartup(StartupEventArgs e) {
+            // Serilog konfigurieren
+            if (!Directory.Exists(AppDomain.CurrentDomain.BaseDirectory + "logs")) {
+                Directory.CreateDirectory(AppDomain.CurrentDomain.BaseDirectory + "logs");
+            }
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.File("logs/log_.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            Log.Information("Application started.");
+
+            base.OnStartup(e);
+
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
+
+        protected override void OnExit(ExitEventArgs e) {
+            Log.Information("Application shutdown.");
+            Log.CloseAndFlush();
+            base.OnExit(e);
+        }
+    }
 }
