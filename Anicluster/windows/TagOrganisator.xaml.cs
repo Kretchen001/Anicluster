5d8239b7b8a9277fbc29ae7166d115519056f7aa
Kretchen001 <83697846+Kretchen001@users.noreply.github.com>
2025-04-07 18:05:18 +0200

    Tag Search implemented

--- Anicluster/windows/TagOrganisator.xaml.cs
+++ Anicluster/windows/TagOrganisator.xaml.cs
@@ -1,9 +1,12 @@
 ﻿using Anicluster.Database;
 using Anicluster.Database.Services;
 using Modells.Anime;
+using Modells.ViewModel;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 using System.Windows.Media;
 
 namespace Anicluster.windows {
@@ -12,17 +15,19 @@ namespace Anicluster.windows {
     /// </summary>
     public partial class TagOrganisator : Window {
 
-        public ObservableCollection<Tag> Tags { get; set; } = new ObservableCollection<Tag>();
+        public ObservableCollection<Tag> AllTags { get; set; } = new ObservableCollection<Tag>();
         public Tag? SelectedTag { get; set; }
         private readonly TagService _tagService = new TagService(new DatabaseManager($"Data Source=db/data.sqlite"));
+        public ICollectionView FilteredTags { get; set; }
 
         /// <summary></summary>
         public TagOrganisator() {
             InitializeComponent();
             foreach (Tag x in _tagService.SelectAllTags()) {
-                Tags.Add(x);
+                AllTags.Add(x);
             }
             DataContext = this;
+            FilteredTags = CollectionViewSource.GetDefaultView(AllTags);
         }
 
         /// <summary></summary>
@@ -36,10 +41,10 @@ namespace Anicluster.windows {
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void BtnPopUp_Click(object sender, RoutedEventArgs e) {
-            if (!Tags.Any(x => x.Designation.Contains(InTxtBoxNewTag.Text, StringComparison.OrdinalIgnoreCase))) {
+            if (!AllTags.Any(x => x.Designation.Contains(InTxtBoxNewTag.Text, StringComparison.OrdinalIgnoreCase))) {
                 Tag temp = new Tag(InTxtBoxNewTag.Text);
                 temp.Id = _tagService.Insert(temp);
-                Tags.Add(temp);
+                AllTags.Add(temp);
                 MessageBox.Show(owner: this, $"Tag '{temp.Designation}' erfolgreich gespeichert!");
                 InTxtBoxNewTag.Text = "";
             }
@@ -52,7 +57,7 @@ namespace Anicluster.windows {
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void InputTextBox_TextChanged(object sender, TextChangedEventArgs e) {
-            if (Tags.Any(x => x.Designation.Equals(InTxtBoxNewTag.Text, StringComparison.OrdinalIgnoreCase))) {
+            if (AllTags.Any(x => x.Designation.Equals(InTxtBoxNewTag.Text, StringComparison.OrdinalIgnoreCase))) {
                 InTxtBoxNewTag.Background = new SolidColorBrush(Color.FromArgb(128, 255, 0, 0)); // A=128 (50% Transparenz), R=255 (Rot), G=0, B=0
             }
             else {
@@ -67,7 +72,7 @@ namespace Anicluster.windows {
             if (SelectedTag is not null) {
                 if (_tagService.DeleteTagById(SelectedTag)) {
                     MessageBox.Show(owner: this, $"Tag '{SelectedTag.Designation}' erfolgreich gelöscht.");
-                    Tags.Remove(SelectedTag);
+                    AllTags.Remove(SelectedTag);
                 }
                 else {
                     MessageBox.Show(owner: this, "Fehler beim Löschen!\nBitte beim Log aufbewahren und Programmierer informieren.");
@@ -76,6 +81,28 @@ namespace Anicluster.windows {
             else {
                 MessageBox.Show(owner: this, "Kein Tag ausgewählt zum Löschen.");
             }
+        }
+
+        private void MenuItemTagSearch_Click(object sender, RoutedEventArgs e) {
+            GrdSearch.Visibility = GrdSearch.IsVisible ? Visibility.Collapsed : Visibility.Visible;
+            MenuItemTagSearch.Header = GrdSearch.IsVisible ? "Suche ausblenden" : "Suche einblenden";
+        }
+
+        private void TxtBxSearchPattern_TextChanged(object sender, TextChangedEventArgs e) {
+            FilteredTags.Filter = obj => {
+                if (obj is Tag tagViewModel) {
+                    return (
+                        string.IsNullOrEmpty(TxtBxSearchPattern.Text) 
+                        || tagViewModel.Designation.Contains(TxtBxSearchPattern.Text, StringComparison.OrdinalIgnoreCase)
+                    );
+                }
+                return false;
+            };
+            FilteredTags.Refresh();
+        }
+
+        private void BtnSearchPatternEmpty_Click(object sender, RoutedEventArgs e) {
+            TxtBxSearchPattern.Text = "";
         }
     }
 }
